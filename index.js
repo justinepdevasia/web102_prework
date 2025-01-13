70a68beeb5bd869a18bc9cbdc77eb48c7e7b4298
justinepdevasia <40311683+justinepdevasia@users.noreply.github.com>
2025-01-12 21:06:00 -0500

    challenge 7 updates

--- index.js
+++ index.js
@@ -190,7 +190,15 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [topGame, runnerUp] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGameElement = document.createElement("p");
+topGameElement.innerHTML = `${topGame.name} - $${topGame.pledged.toLocaleString()}`;
+firstGameContainer.appendChild(topGameElement);
+
 
 // do the same for the runner up item
+const runnerUpElement = document.createElement("p");
+runnerUpElement.innerHTML = `${runnerUp.name} - $${runnerUp.pledged.toLocaleString()}`;
+secondGameContainer.appendChild(runnerUpElement);
