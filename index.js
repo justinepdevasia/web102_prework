2d2708a129abb4f7879422b57129aafa93db89c7
justinepdevasia <40311683+justinepdevasia@users.noreply.github.com>
2025-01-12 20:31:12 -0500

    challenge 6 updates

--- index.js
+++ index.js
@@ -158,12 +158,24 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
-
+const numUnfundedGames = GAMES_JSON.filter( game => {
+    return game.pledged < game.goal;
+}).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
+const unfundedString = numUnfundedGames === 0 ? "all" : numUnfundedGames;
 
+const totalRaised = GAMES_JSON.reduce((acc, game) => {
+    return acc + game.pledged;
+}, 0);
+
+const descriptionString = `A total of $${totalRaised.toLocaleString()} has been raised for ${GAMES_JSON.length} games. Currently, ${numUnfundedGames === 0 ? "all" : numUnfundedGames} games remain unfunded. We need your help to fund these amazing games!`;
 
 // create a new DOM element containing the template string and append it to the description container
+const description = document.createElement("p");
+description.innerHTML = descriptionString;
+descriptionContainer.appendChild(description);
+
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
